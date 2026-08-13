4277e82922cd36d9f21c802249136e26d8f7fa29
Huy-DNA <huydo862003@gmail.com>
2026-08-13 19:07:58 +0700

    fix: scroll page to top on next and prev

--- packages/typerighter/src/client/app/router.ts
+++ packages/typerighter/src/client/app/router.ts
@@ -100,7 +100,11 @@ export function createRouter (
 
         if (isInBrowser) {
           nextTick(() => {
-            scrollToHash(targetLocation.hash);
+            if (targetLocation.hash) {
+              scrollToHash(targetLocation.hash);
+            } else {
+              window.scrollTo(0, 0);
+            }
           });
         }
       }
