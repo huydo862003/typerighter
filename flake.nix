d6593073dbb245ffb31ca938f85b15f18149732c
Huy-DNA <huydo862003@gmail.com>
2026-08-26 17:15:03 +0700

    chore: release v0.15.1

--- flake.nix
+++ flake.nix
@@ -65,7 +65,7 @@
         };
         typedown-server = rustPlatform.buildRustPackage {
           pname = "typedown-server";
-          version = "0.15.0";
+          version = "0.15.1";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
           cargoBuildFlags = [
